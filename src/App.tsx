1b7fb540a76fcb466d882cd455e065add3c7a7e9
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-06-04 17:48:19 -0300

    first commit web

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import './App.css';
+
+const App: React.FC = () => {
+  return <Routes />;
+};
+
+export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
+const Routes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/create-point" component={CreatePoint} />
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
