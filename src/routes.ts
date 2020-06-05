8288a6a9b73218955a308cf86415f799fd9d38ce
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-06-05 15:11:18 -0300

    data validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 
 import multerConfig from './config/multer';
 
@@ -24,7 +25,26 @@ routes.get('/', (request, response) => {
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false },
+  ),
+  pointsController.create,
+);
 
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 
@@ -10,5 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(process.env.PORT || 3333);
