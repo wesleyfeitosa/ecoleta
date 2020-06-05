de2923078e3e26d80daa50b579df566a3b91da0d
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-06-05 15:20:20 -0300

    adjustment on validate

--- src/routes.ts
+++ src/routes.ts
@@ -33,9 +33,9 @@ routes.post(
       body: Joi.object().keys({
         name: Joi.string().required(),
         email: Joi.string().required(),
-        whatsapp: Joi.number().required(),
-        latitude: Joi.number().required(),
-        longitude: Joi.number().required(),
+        whatsapp: Joi.required(),
+        latitude: Joi.required(),
+        longitude: Joi.required(),
         city: Joi.string().required(),
         uf: Joi.string().required().max(2),
         items: Joi.string().required(),
